8f10d758e45e5bea8ba5b8909cce0ef17acefd6b
Matheus Araújo <matheus.daf@outlook.com>
2022-07-06 00:58:42 -0300

    fix/color fill

--- src/components/LoadingCircle.jsx
+++ src/components/LoadingCircle.jsx
@@ -3,7 +3,7 @@ export function LoadingCircle() {
     <div className="flex justify-center items-center h-screen">
       <svg
         role="status"
-        className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 f"
+        className="w-8 h-8 mr-2 text-gray-100 animate-spin dark:text-gray-400 fill-[#862924] f"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
