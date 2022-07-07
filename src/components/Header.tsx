f0bf932d3b96532dc5f6d027fedbb15f4bc5bcc0
Matheus Araújo <matheus.daf@outlook.com>
2022-07-07 19:30:46 -0300

    fix 404 error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,13 @@ export function Header(props: HeaderProps) {
   return (
     <header className="w-full bg-[#862924] max-h-14 h-14 shadow-4xl">
       {props.hasIcon && (
-        <div className="flex h-full justify-end items-center mr-4">
-          <GearSix size={24} color="#ffffff" weight="fill" className="" />
+        <div className="flex h-full justify-end items-center  ">
+          <GearSix
+            size={24}
+            color="#ffffff"
+            weight="fill"
+            className="cursor-pointer mr-4"
+          />
         </div>
       )}
     </header>
