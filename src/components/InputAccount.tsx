87a46e79bb93c32ec7ec1eddb974002a9b054ccf
Matheus Araújo <matheus.daf@outlook.com>
2022-07-23 16:22:37 -0300

    loading circle from signup removed | input disabled in loading

--- src/components/InputAccount.tsx
+++ src/components/InputAccount.tsx
@@ -8,6 +8,7 @@ interface DataProps {
 export function InputAccount(props: DataProps) {
   return (
     <input
+      disabled={props.disabled}
       type="submit"
       value={props.value}
       className={`${props.class} rounded-3xl text-${props.size} cursor-pointer uppercase`}
--- src/pages/Account/Signup.tsx
+++ src/pages/Account/Signup.tsx
@@ -88,107 +88,103 @@ export function Signup() {
 
   return (
     <>
-      {loading === true ? (
-        <>{<LoadingCircle />}</>
-      ) : (
-        <>
-          <form
-            className="lg:mt-12 sans max-w-lg lg:m-auto sm:m-auto sm:items-center"
-            onSubmit={handleUser}
-          >
-            <Logo name="Signup" />
+      <>
+        <form
+          className="lg:mt-12 sans max-w-lg lg:m-auto sm:m-auto sm:items-center"
+          onSubmit={handleUser}
+        >
+          <Logo name="Signup" />
 
-            <div className="flex flex-col m-auto items-center w-[358px]">
-              <div className="mt-12 mb-4">
-                <label htmlFor="name" className="block text-sm text-[#424242]">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  placeholder="Full Name"
-                  className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
-                  onChange={(e) => {
-                    setName(e.target.value);
-                  }}
-                  maxLength={40}
-                />
-              </div>
-
-              <div className="mb-4">
-                <label htmlFor="name" className="block text-sm text-[#424242]">
-                  Phone Number
-                </label>
-
-                <InputMask
-                  mask="(99) 9 9999-9999"
-                  placeholder="(00) 0 0000-0000"
-                  className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
-                  onChange={(e) => {
-                    setPhone(e.target.value);
-                  }}
-                />
-              </div>
-
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm text-[#424242] mb-[10px]"
-                >
-                  Email
-                </label>
-
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Enter email..."
-                  className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] "
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </div>
-
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm text-[#424242]"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Enter password..."
-                  className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  maxLength={11}
-                  minLength={6}
-                />
-              </div>
-
-              <InputAccount
-                disabled={loading}
-                value="signup"
-                size="sm"
-                class="bg-orange-900 h-10 mt-9 text-white w-[358px] disabled:opacity-50"
+          <div className="flex flex-col m-auto items-center w-[358px]">
+            <div className="mt-12 mb-4">
+              <label htmlFor="name" className="block text-sm text-[#424242]">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Full Name"
+                className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                maxLength={40}
               />
             </div>
-            <NavLink
-              to="/login"
-              className="flex justify-center pb-5 800tall:absolute 800tall:bottom-0 800tall:right-0 800tall:left-0"
-            >
-              <InputAccount
-                value="already have an account?"
-                size="sm"
-                class="w-[358px] h-9 mt-9 text-[#999999] font-bold border tracking-widest "
+
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-sm text-[#424242]">
+                Phone Number
+              </label>
+
+              <InputMask
+                mask="(99) 9 9999-9999"
+                placeholder="(00) 0 0000-0000"
+                className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
               />
-            </NavLink>
-          </form>
-        </>
-      )}
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm text-[#424242] mb-[10px]"
+              >
+                Email
+              </label>
+
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter email..."
+                className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] "
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm text-[#424242]"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter password..."
+                className="border rounded-[4.5px] pl-[10px] h-10 w-[358px] mt-[10px]"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                maxLength={11}
+                minLength={6}
+              />
+            </div>
+
+            <InputAccount
+              disabled={loading}
+              value="signup"
+              size="sm"
+              class="bg-orange-900 h-10 mt-9 text-white w-[358px] disabled:opacity-60"
+            />
+          </div>
+          <NavLink
+            to="/login"
+            className="flex justify-center pb-5 800tall:absolute 800tall:bottom-0 800tall:right-0 800tall:left-0"
+          >
+            <InputAccount
+              value="already have an account?"
+              size="sm"
+              class="w-[358px] h-9 mt-9 text-[#999999] font-bold border tracking-widest "
+            />
+          </NavLink>
+        </form>
+      </>
     </>
   );
 }
